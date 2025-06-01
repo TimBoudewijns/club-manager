4419e12a46210e724d3d1c6bfb1e009775df1703
Tim Boudewijns <tim@troublefree.nl>
2025-06-01 10:01:33 +0200

    Fix color change 7

--- includes/class-club-manager-shortcode.php
+++ includes/class-club-manager-shortcode.php
@@ -39,29 +39,31 @@ class Club_Manager_Shortcode {
             
             // Configure Tailwind IMMEDIATELY with correct orange colors
             wp_add_inline_script('tailwind-css', '
-                tailwind.config = {
-                    darkMode: "class",
-                    theme: {
-                        extend: {
-                            colors: {
-                                orange: {
-                                    50: "#fff7ed",
-                                    100: "#ffedd5",
-                                    200: "#fed7aa",
-                                    300: "#fdba74",
-                                    400: "#fb923c",
-                                    500: "#f97316",
-                                    600: "#ea580c",
-                                    700: "#c2410c",
-                                    800: "#9a3412",
-                                    900: "#7c2d12",
-                                    950: "#431407"
+                if (typeof tailwind !== "undefined") {
+                    tailwind.config = {
+                        darkMode: "class",
+                        theme: {
+                            extend: {
+                                colors: {
+                                    orange: {
+                                        50: "#fff7ed",
+                                        100: "#ffedd5",
+                                        200: "#fed7aa",
+                                        300: "#fdba74",
+                                        400: "#fb923c",
+                                        500: "#f97316",
+                                        600: "#ea580c",
+                                        700: "#c2410c",
+                                        800: "#9a3412",
+                                        900: "#7c2d12",
+                                        950: "#431407"
+                                    }
                                 }
                             }
                         }
-                    }
-                };
-            ', 'before');
+                    };
+                }
+            ', 'after');
             
             // Enqueue DaisyUI
             wp_enqueue_style(
